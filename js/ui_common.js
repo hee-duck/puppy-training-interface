2444c3dcdd9c1658a978781719a54423ce9e3ccf
homin <hominin@naver.com>
2023-03-28 13:03:02 +0900

    sns해줘!

---
+++ js/ui_common.js
@@ -0,0 +1,18 @@
+  var swiper = new Swiper(".mySwiper", {
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    effect: "fade",
+    loop: true,
+  });
+
+
