737cce51fd3fbb86984e13f5ce5b8a1ffa75e864
juyeon <perbell521@gmail.com>
2023-03-28 18:09:40 +0900

    주바리 오늘 할당량

---
+++ js/accordian.js
@@ -0,0 +1,7 @@
+
+$(".que").click(function() {
+    $(this).next(".anw").stop().slideToggle(300);
+   $(this).toggleClass('on').siblings().removeClass('on');
+   $(this).next(".anw").siblings(".anw").slideUp(300); // 1개씩 펼치기
+ });
+
