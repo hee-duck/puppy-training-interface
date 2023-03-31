7e8a180b35e1b0569fc52df1585d31d74c4a3ed2
heesoo <juntu09@gmail.com>
2023-03-31 11:00:13 +0900

    header hambuger 아코디언 추가

---
+++ js/burger.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    // 처음 모든 ol(하위메뉴) 숨김 처리
+    $('.slide-menu li ol').hide();
+    
+    // li에 이벤트 핸들러 추가
+    $('.slide-menu li').click(function(){
+      // 태그 츨릭하면 모든 ol 태그 나타남
+      $('.slide-menu li ol').not($(this).children('ol')).slideUp();
+      
+      // 클릭한 li 태그의 자식 ol 태그 클릭
+      $(this).children('ol').slideToggle();
+    });
+  });
+  
