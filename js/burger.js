7014b8db78ceeeded4ac3e939c2ed20116bb4032
heesoo <juntu09@gmail.com>
2023-04-03 11:11:33 +0900

    햄버거 아코디언 stopPropagation 트리거 추가

--- js/burger.js
+++ js/burger.js
@@ -1,4 +1,4 @@
-// 상위메뉴 누르면 하위메뉴 아코디언 
+/* // 상위메뉴 누르면 하위메뉴 아코디언 ===> 아코디언만 되고 ol 요소 링크 전환이 안되서 주석처리
 $(document).ready(function () {
   // 처음 모든 ol(하위메뉴) 숨김 처리
   $('.slide-menu li ol').hide();
@@ -12,6 +12,8 @@ $(document).ready(function () {
     $(this).children('ol').slideToggle();
   });
 });
+ */
+
 
 // 슬라이드 오픈
 $(function () {
@@ -26,6 +28,25 @@ $(function () {
     }
   });
 });
+
+
+$(document).ready(function() {
+  // 처음 모든 ol(하위메뉴) 숨김 처리
+  $('li ol').hide();
+  
+  // li를 클릭하면 ol 펼치기, 접기
+  $('li').click(function() {
+    $(this).children('ol').slideToggle();
+  });
+  
+  // ol 클릭하면 a 링크로 연결 (이벤트 추가)
+  $('li ol a').click(function(event) {
+    event.stopPropagation(); //stopPropagation : 클릭 이벤트가 li 요소로 버블링되어 토글 기능을 트리거하는걸 막음
+    window.location.href = $(this).attr('href');
+  });
+});
+
+
 
 // 딤드 처리(처음엔 display none 햄버거 버튼 누르면 모바일gnb 제외 영역 딤드처리, 버튼 다시 클릭하면 none
 const dimmed = document.querySelector('.dimmed');
