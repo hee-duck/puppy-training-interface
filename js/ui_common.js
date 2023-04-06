b02d7a668b9bccefe4f3d5ceb57268c00580346d
homin <hominin@naver.com>
2023-04-06 16:15:57 +0900

    동적움직임1

--- js/ui_common.js
+++ js/ui_common.js
@@ -16,3 +16,24 @@
   });
 
 
+  // edu_info 움직임
+  $(document).ready(function(){
+    $(window).scroll(function(){
+        var hei = $(document).scrollTop();
+        if(hei > 300){
+            $('.edu_info_txt').addClass('on')
+        }
+    });
+  });
+
+  // edu_join움직임
+  $(document).ready(function(){
+    $(window).scroll(function(){
+        var hei = $(document).scrollTop();
+        if(hei > 700){
+            $('.edu_join_inner').children().addClass('on')
+        }
+    });
+  });
+
+
